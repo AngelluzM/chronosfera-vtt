dd75b83bec38353a8dc6cd94e4dec5b13b46d515
AngelluzM <4ng3l0uc0@gmail.com>
2026-05-26 19:03:22 -0300

    Update chronosfera.mjs

--- module/chronosfera.mjs
+++ module/chronosfera.mjs
@@ -1,12 +1,16 @@
+import { ChronosferaActor } from "./documents/actor.mjs";
 import { ChronosferaPersonagemSheet } from "./sheets/personagem-sheet.mjs";
 
 Hooks.once("init", function() {
     console.log("Chronosfera RPG | Inicializando a Linha do Tempo...");
 
-    // Remove a ficha padrão feia do Foundry
+    // Registra o nosso processador de dados matemáticos customizado
+    CONFIG.Actor.documentClass = ChronosferaActor;
+
+    // Desregistra as fichas padrões do sistema do Foundry
     Actors.unregisterSheet("core", ActorSheet);
     
-    // Registra a nossa ficha bonita!
+    // Registra a folha visual Handlebars
     Actors.registerSheet("chronosfera", ChronosferaPersonagemSheet, {
         types: ["personagem"],
         makeDefault: true
